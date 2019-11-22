d6f0ae3a520cdc2a288153afca9f74fc35d6a0d1
Belmin Fernandez <belminf@gmail.com>
2019-11-22 13:58:59 -0500

    Cleans up config for tpm

--- session_attach.sh
+++ session_attach.sh
@@ -7,14 +7,13 @@ TMUX_SESSION_GROUP="term"
 set -e
 
 # Check if any unattached session
-UNATTACHED_SESSION="$(tmux list-session | egrep "\(group ${TMUX_SESSION_GROUP}\)$" | head -n 1 | cut -d':' -f1)"
+UNATTACHED_SESSION="$(tmux list-session | grep -e "\(group ${TMUX_SESSION_GROUP}\)$" | head -n 1 | cut -d':' -f1)"
 
 # If we found an unattached session, attach to it
-if [ -n "$UNATTACHED_SESSION" ]
-then
-  tmux attach-session -t $UNATTACHED_SESSION
+if [ -n "$UNATTACHED_SESSION" ]; then
+  tmux attach-session -t "$UNATTACHED_SESSION"
 
 # Otherwise, create a new session in the group
 else
-  tmux new-session -t $TMUX_SESSION_GROUP
+  tmux new-session -t "$TMUX_SESSION_GROUP"
 fi
