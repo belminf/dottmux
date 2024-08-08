246accbe701636a06102b055980c38c669c0656b
Belmin Fernandez <belminf@gmail.com>
2024-08-08 00:13:54 -0400

    Cleans up status line

--- status-plugins/world_clock.sh
+++ status-plugins/world_clock.sh
@@ -2,10 +2,10 @@ show_world_clock() {
   local index icon color text module
 
   index="$1"
-  icon="$(get_tmux_option "@catppuccin_date_time_icon" "󰃰")"
+  icon="$(get_tmux_option "@catppuccin_date_time_icon" "󰥔")"
   color="$(get_tmux_option "@catppuccin_date_time_color" "$thm_blue")"
 
-  text='#(TZ="America/Los_Angeles" date +"%%H:%%M %%Z") | #(date -u +"%%H:%%M %%Z")'
+  text='#(TZ="America/Los_Angeles" date +"%%H:%%M %%Z")  #(date -u +"%%H:%%M %%Z")'
 
   module=$(build_status_module "$index" "$icon" "$color" "$text")
 
